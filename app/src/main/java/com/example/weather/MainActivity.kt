85099138c3f78160ade2ad3b49e101f2f92af990
k-key-k <vaizat01@gmail.com>
2023-11-15 02:08:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.weather
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weather.ui.theme.WeatherTheme
+import org.json.JSONObject
+
+const val API_KEY = "1b73d46ce4d44f12980131902232610"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Kazan", this)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, context: Context){
+
+    val state = remember { mutableStateOf("Неопределенный населённый пункт...") }
+
+    Column(modifier = Modifier.fillMaxWidth())
+    {
+        Box (
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        )
+        {
+            Text(text = "Temp in $name ${state.value} C")
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentAlignment = Alignment.BottomCenter
+        )
+        {
+            Button(
+                onClick = {
+                    getResult(name, state, context)
+                },
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+            )
+            {
+                Text(text = "Обновить")
+            }
+        }
+    }
+}
+
+private fun getResult(city: String, state: MutableState<String>, context: Context){
+    val url = "https://api.weatherapi.com/v1/current.json" +
+            "?key=$API_KEY" +
+            "&q=$city" +
+            "&aqi=no"
+
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+            response ->
+            val obj=JSONObject(response)
+            state.value = obj.getJSONObject("current").getString("temp_c")
+        },
+        {
+            error -> Log.d("Sh1", "Error $error")
+        }
+    )
+    queue.add(stringRequest)
+}
